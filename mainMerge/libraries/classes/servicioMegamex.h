588d95920553d0d83473d93c2367bd5df42beec9
Antonio Hernandez <ahm@lincolnlaw.com>
2016-11-30 18:06:46 -0700

    AH-006: Modified class "Servicios" to be an array of 10 elements

--- mainMerge/libraries/classes/servicioMegamex.h
+++ mainMerge/libraries/classes/servicioMegamex.h
@@ -19,80 +19,102 @@ class servicios
     float costoServicio, comisionEmpleado;
 
     // Metodos publicos de la clase servicio.
-    void capturar();
-    void mostrar();
-    void buscar();
-    int menu();
-} sv;
+    static void capturar();
+    static void mostrar();
+    static void buscar();
+    static int menu();
+} sv[10];
 
 /**
  * Recopilamos los datos del servicio.
  */
 void servicios::capturar()
 {
-  cout << "--INGRESE DATOS DEL SERVICIO--" << endl << endl;
+  int amountToRegister = 0;
 
-  // Limpiamos bufferde cualquier caracter indeseado.
-  cin.ignore(1, '\n');
+  if ((10 - serviciosDisp) > 0) {
+    cout << "10 - amountToRegister es " << (10 - amountToRegister) << endl;
+    cout << "Inserte el numero de servicios que desea capturar (Maximo " << (10 - serviciosDisp) << "): ";
+    cin >> amountToRegister;
+    checkInputRange(amountToRegister, 1, (10 - serviciosDisp));
 
-  cout << "Ingresa el titulo de su servicio (Inserta 'help' para ejemplos de posibles titulos): ";
-  getline(cin, sv.tituloServicio);
+    for (int i = 0; i < amountToRegister; ++i) {
+      cout << "Aqui i vale " << i << endl;
+      cout << "Aqui amountToRegister vale " << amountToRegister << endl;
+      cout << "-- INGRESE LOS DATOS DEL SERVICIO " << (i + 1) << " --" << endl;
 
-    // Implementacion de ejemplos para nombramiento de titulo.
-    if (sv.tituloServicio == "help") {
-      int i = 0;
+      // Limpiamos buffer de cualquier caracter indeseado.
+      cin.sync();
 
-      while (sv.tituloServicio == "help") {
-         switch (i) {
-           case 0:
-           cout << "  p. ej. Paquete Televisivo Basico 1." << endl;
-           break;
-         case 1:
-           cout << "  p. ej. Paquete Economico." << endl;
-           break;
-         case 2:
-           cout << "p. ej. Megapaquete Cable/Internet/Telefono." << endl;
-           i = -1;
-           break;
-        }
+      cout << "Ingresa el titulo de su servicio (Inserta 'help' para ejemplos de posibles titulos): ";
+      getline(cin, sv[i].tituloServicio);
 
-        i++;
+      // Implementacion de ejemplos para nombramiento de titulo.
+      if (sv[i].tituloServicio == "help") {
+        int i = 0;
 
-        cout << endl << "Titulo de servicio: ";
-        getline(cin, sv.tituloServicio);
-     }
-   }
+        while (sv[i].tituloServicio == "help") {
+          switch (i) {
+            case 0:
+              cout << "  p. ej. Paquete Televisivo Basico 1." << endl;
+              break;
+            case 1:
+             cout << "  p. ej. Paquete Economico." << endl;
+             break;
+            case 2:
+             cout << "p. ej. Megapaquete Cable/Internet/Telefono." << endl;
+             i = -1;
+             break;
+          }
 
-  cout << endl << "Especifique el digito del tipo de servicio a ofrecer." << endl;
-  cout << "  1.- Telefonia Digital." << endl;
-  cout << "  2.- Television Digital." << endl;
-  cout << "  3.- Television Satelital." << endl;
-  cout << "  4.- Internet" << endl;
-  cout << "  5.- Mixto (Especificar en descripcion del servicio)."  << endl;
-  cout << endl <<  "  Tipo de Servicio: ";
-  cin >> sv.tipoServicio;
-  checkInputRange(sv.tipoServicio, 1, 5);
+          i++;
 
-  cout << endl << "Ingrese el codigo de tu servicio (Asegurate de que el codigo"
-  << " tenga un maximo de 5 digitos): ";
-  // @TODO: Revisar si codigo de servicio ingresado ya existe.
-  cin >> sv.codigoServicio;
-  checkInputLength(sv.codigoServicio, 5);
+          cout << endl << "Titulo de servicio: ";
+          getline(cin, sv[i].tituloServicio);
+       }
+      }
 
-  // @TODO: Revisar que cantidad ingresada sea valida.
-  cout << endl << "Ingrese el costo del servicio:" << endl;
-  cout << "  $ ";
-  cin >> sv.costoServicio;
+      cout << endl << "Especifique el digito del tipo de servicio a ofrecer." << endl;
+      cout << "  1.- Telefonia Digital." << endl;
+      cout << "  2.- Television Digital." << endl;
+      cout << "  3.- Television Satelital." << endl;
+      cout << "  4.- Internet" << endl;
+      cout << "  5.- Mixto (Especificar en descripcion del servicio)."  << endl;
+      cout << endl <<  "  Tipo de Servicio: ";
+      cin >> sv[i].tipoServicio;
+      checkInputRange(sv[i].tipoServicio, 1, 5);
 
-  cout << endl << "Ingrese la comision del empleado por el servicio:" << endl;
-  cout << "  $ ";
-  cin >> sv.comisionEmpleado;
+      cout << endl << "Ingrese el codigo de tu servicio (Asegurate de que el codigo"
+      << " tenga un maximo de 5 digitos): ";
+      // @TODO: Revisar si codigo de servicio ingresado ya existe.
+      cin >> sv[i].codigoServicio;
+      checkInputLength(sv[i].codigoServicio, 5);
 
-  // Limpiamos buffer para poder registrar la descripcion del servicio.
-  cin.ignore(1, '\n');
+      // @TODO: Revisar que cantidad ingresada sea valida.
+      cout << endl << "Ingrese el costo del servicio:" << endl;
+      cout << "  $ ";
+      cin >> sv[i].costoServicio;
 
-  cout << endl << "Ingrese una breve descripcion del servicio: ";
-  getline(cin, sv.descripcionServicio);
+      cout << endl << "Ingrese la comision del empleado por el servicio:" << endl;
+      cout << "  $ ";
+      cin >> sv[i].comisionEmpleado;
+
+      // Limpiamos buffer para poder registrar la descripcion del servicio.
+      cin.ignore(1, '\n');
+
+      cout << endl << "Ingrese una breve descripcion del servicio: ";
+      getline(cin, sv[i].descripcionServicio);
+
+      ++serviciosDisp;
+
+      pause();
+    }
+  }
+
+  else {
+    cout << "Imposible agregar mas servicios" << endl;
+    return;
+  }
 
   cout << endl;
 }
@@ -102,34 +124,36 @@ void servicios::capturar()
  */
 void servicios::mostrar()
 {
-  cout << "--DATOS DEL SERVICIO--" << endl;
+  for (int i = 0; i < serviciosDisp; ++i) {
+    cout << "-- DATOS DEL SERVICIO " << (i + 1) << " --" << endl;
 
-  cout << "Titulo: " << sv.tituloServicio << endl;
+    cout << "Titulo: " << sv[i].tituloServicio << endl;
 
-  cout << "Tipo: ";
-  switch (sv.tipoServicio) {
-    case 1:
-      cout << "Telefonia Digital.";
-      break;
-    case 2:
-      cout << "Television Digital.";
-      break;
-    case 3:
-      cout << "Television Satelital.";
-      break;
-    case 4:
-      cout << "Internet.";
-      break;
-    case 5:
-      cout << "Mixto.";
-      break;
+    cout << "Tipo: ";
+    switch (sv[i].tipoServicio) {
+      case 1:
+        cout << "Telefonia Digital.";
+        break;
+      case 2:
+        cout << "Television Digital.";
+        break;
+      case 3:
+        cout << "Television Satelital.";
+        break;
+      case 4:
+        cout << "Internet.";
+        break;
+      case 5:
+        cout << "Mixto.";
+        break;
+    }
+    cout << endl;
+
+    cout << "Codigo: " << sv[i].codigoServicio << endl;
+    cout << "Costo: $" << sv[i].costoServicio << endl;
+    cout << "Comision de empleado: $" << sv[i].comisionEmpleado << endl;
+    cout << "Descripcion: " << sv[i].descripcionServicio << endl << endl;
   }
-  cout << endl;
-
-  cout << "Codigo: " << sv.codigoServicio << endl;
-  cout << "Costo: $" << sv.costoServicio << endl;
-  cout << "Comision de empleado: $" << sv.comisionEmpleado << endl;
-  cout << "Descripcion: " << sv.descripcionServicio << endl;
 }
 
 void servicios::buscar()
@@ -143,18 +167,25 @@ void servicios::buscar()
     checkInputLength(codigo, 5);
     cout << endl;
 
-    if (sv.codigoServicio == codigo) {
-      cout << "Titulo de servicio: ";
-      cout << sv.tituloServicio << endl;
+    for (int i = 0; i < serviciosDisp; ++i) {
+      if (sv[i].codigoServicio == codigo) {
+        cout << "Titulo de servicio: ";
+        cout << sv[i].tituloServicio << endl;
 
-      cout << "Costo de servicio: ";
-      cout << sv.costoServicio << endl ;
+        cout << "Costo de servicio: ";
+        cout << sv[i].costoServicio << endl ;
 
-      cout << "Comision a empleado: ";
-      cout << sv.comisionEmpleado << endl ;
-    }
-    else {
-      cout << "Servicio solicitado inexistente." << endl << endl;
+        cout << "Comision a empleado: ";
+        cout << sv[i].comisionEmpleado << endl ;
+      }
+
+      if (i == (serviciosDisp - 1)) {
+        cout << "Servicio inexistente." << endl;
+      }
+
+      else {
+        continue;
+      }
     }
 
     cout << "Quieres revisar otro producto?" << endl;
@@ -185,15 +216,15 @@ int servicios::menu()
 
     switch (eleccion) {
       case 1:
-        sv.capturar();
+        servicios::capturar();
         cout << endl;
         break;
       case 2:
-        sv.mostrar();
+        servicios::mostrar();
         cout << endl;
         break;
       case 3:
-        sv.buscar();
+        servicios::buscar();
         cout << endl;
         break;
       case 4:
